41b8ff6dfe921d8c1af7071f66bf9a49297dce92
TomiSzabo00 <tamas.szabo0102@gmail.com>
2022-10-26 18:51:08 +0200

    scores screen done

--- lib/screens/scores_screen.dart
+++ lib/screens/scores_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:tiszapp_flutter/colors.dart';
 import 'package:tiszapp_flutter/data/score_data.dart';
 import 'package:tiszapp_flutter/widgets/score_item.dart';
 
@@ -20,7 +21,7 @@ class _ScoresScreenState extends State<ScoresScreen> {
     scoresRef = FirebaseDatabase.instance.ref().child("debug/scores");
     scoresRef.onChildAdded.listen((event) {
       setState(() {
-        scores.add(Score.fromSnapshot(event.snapshot));
+        scores.insert(0, Score.fromSnapshot(event.snapshot));
       });
     });
     super.initState();
@@ -28,16 +29,68 @@ class _ScoresScreenState extends State<ScoresScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isDarkTheme = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pontállás'),
-      ),
-      body: ListView.builder(
-        itemCount: scores.length,
-        itemBuilder: (context, index) {
-          return ScoreItem(scoreData: scores[index]);
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Pontállás'),
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  isDarkTheme ? 'images/bg2_night.png' : 'images/bg2_day.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    const SizedBox(
+                      width: 120,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: CustomColor.semiTransparentWhite,
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: const Text(
+                        'Csapatok pontjai',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ScoreItem(
+                scoreData: Score(
+                    author: "",
+                    name: 'Program neve',
+                    score1: 1,
+                    score2: 2,
+                    score3: 3,
+                    score4: 4),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: scores.length,
+                  itemBuilder: (context, index) {
+                    return ScoreItem(scoreData: scores[index]);
+                  },
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/score_item.dart
+++ lib/widgets/score_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiszapp_flutter/colors.dart';
 import 'package:tiszapp_flutter/data/score_data.dart';
 
 class ScoreItem extends StatelessWidget {
@@ -9,38 +10,82 @@ class ScoreItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(5),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(
-            scoreData.name,
-            style: const TextStyle(
-              fontSize: 20,
+          Container(
+            width: 120,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: CustomColor.semiTransparentWhite,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Text(
+              scoreData.name,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
             ),
           ),
-          Text(
-            scoreData.score1.toString(),
-            style: const TextStyle(
-              fontSize: 20,
+          Container(
+            width: (MediaQuery.of(context).size.width - 120 - 20 - 20) / 4,
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: CustomColor.semiTransparentWhite,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Text(
+              scoreData.score1.toString(),
+              style: const TextStyle(
+                fontSize: 20,
+              ),
             ),
           ),
-          Text(
-            scoreData.score2.toString(),
-            style: const TextStyle(
-              fontSize: 20,
+          Container(
+            width: (MediaQuery.of(context).size.width - 120 - 20 - 20) / 4,
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: CustomColor.semiTransparentWhite,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Text(
+              scoreData.score2.toString(),
+              style: const TextStyle(
+                fontSize: 20,
+              ),
             ),
           ),
-          Text(
-            scoreData.score3.toString(),
-            style: const TextStyle(
-              fontSize: 20,
+          Container(
+            width: (MediaQuery.of(context).size.width - 120 - 20 - 20) / 4,
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: CustomColor.semiTransparentWhite,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Text(
+              scoreData.score3.toString(),
+              style: const TextStyle(
+                fontSize: 20,
+              ),
             ),
           ),
-          Text(
-            scoreData.score4.toString(),
-            style: const TextStyle(
-              fontSize: 20,
+          Container(
+            width: (MediaQuery.of(context).size.width - 120 - 20 - 20) / 4,
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: CustomColor.semiTransparentWhite,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Text(
+              scoreData.score4.toString(),
+              style: const TextStyle(
+                fontSize: 20,
+              ),
             ),
           ),
         ],
