15ecb70d3c4762292365b41081c81c40e7353e4b
TomiSzabo00 <tamas.szabo0102@gmail.com>
2022-10-26 23:29:14 +0200

    load 1 image

--- lib/screens/pictures_screen.dart
+++ lib/screens/pictures_screen.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
-class PicturesScreen extends StatelessWidget {
+class PicturesScreen extends StatefulWidget {
   const PicturesScreen({super.key});
+
+  @override
+  State<PicturesScreen> createState() => _PicturesScreenState();
+}
+
+class _PicturesScreenState extends State<PicturesScreen> {
+  final DatabaseReference picsRef =
+      FirebaseDatabase.instance.ref().child("debug/pics");
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Képek"),
+        title: const Text('Képek'),
       ),
-      body: const Center(
-        child: Text("Képek"),
+      body: StreamBuilder(
+        stream: picsRef.onChildAdded,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final picRef = (snapshot.data!.snapshot.value as Map)['fileName'];
+            print(picRef);
+            return FutureBuilder(
+              future: FirebaseStorage.instance
+                  .ref()
+                  .child('debug/$picRef.png')
+                  .getDownloadURL(),
+              builder: (context, url) {
+                if (url.hasData) {
+                  return Image.network(url.data!);
+                }
+                return const Text('Loading...');
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
