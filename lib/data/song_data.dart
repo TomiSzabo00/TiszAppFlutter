4b3560f47faa53fc837fa25f9629bff129a3f978
TomiSzabo00 <tamas.szabo0102@gmail.com>
2022-10-31 11:15:43 +0100

    load songs from asstest

---
+++ lib/data/song_data.dart
@@ -0,0 +1,6 @@
+class Song {
+  final String name;
+  final String lyrics;
+
+  Song({required this.name, required this.lyrics});
+}
--- lib/screens/songs_screen.dart
+++ lib/screens/songs_screen.dart
@@ -1,7 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class SongsScreen extends StatelessWidget {
   const SongsScreen({super.key});
+
+  Future<String> loadSongs() async {
+    final data = await rootBundle.loadString('assets/metadata/names.txt');
+    List<String> lines = data.split('\n');
+    List<String> songs = [];
+    for (String line in lines) {
+      songs.add('assets/$line');
+    }
+
+    return await rootBundle.loadString(songs[0]);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +24,17 @@ class SongsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Dalok"),
       ),
-      body: const Center(
-        child: Text("Dalok"),
+      body: FutureBuilder(
+        future: loadSongs(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
