66992831e89b3aeeba8cc500eebf29a8b952169a
TomiSzabo00 <tamas.szabo0102@gmail.com>
2022-10-31 23:41:28 +0100

    load songs into model and display titles

--- lib/data/song_data.dart
+++ lib/data/song_data.dart
@@ -1,6 +1,7 @@
 class Song {
   final String name;
   final String lyrics;
+  final String assetPath;
 
-  Song({required this.name, required this.lyrics});
+  Song({required this.name, required this.lyrics, required this.assetPath});
 }
--- lib/screens/songs_screen.dart
+++ lib/screens/songs_screen.dart
@@ -1,34 +1,57 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:tiszapp_flutter/data/song_data.dart';
 
-class SongsScreen extends StatelessWidget {
-  const SongsScreen({super.key});
+class SongsScreen extends StatefulWidget {
+  const SongsScreen({Key? key}) : super(key: key);
 
-  Future<String> loadSongs() async {
+  @override
+  SongsScreenState createState() => SongsScreenState();
+}
+
+class SongsScreenState extends State<SongsScreen> {
+  final List<Song> _songs = [];
+
+  Future<void> loadSongs() async {
     final data = await rootBundle.loadString('assets/metadata/names.txt');
     List<String> lines = data.split('\n');
-    List<String> songs = [];
     for (String line in lines) {
-      songs.add('assets/$line');
+      final assetPath = _getAssetPathFromFile(line);
+      _songs.add(Song(
+          name: _getTitleFromFile(line),
+          lyrics: await rootBundle.loadString(assetPath),
+          assetPath: assetPath));
     }
+    //return await rootBundle.loadString(songs[0]);
+  }
 
-    return await rootBundle.loadString(songs[0]);
+  String _getTitleFromFile(String file) {
+    //remove .txt from string
+    return file.substring(0, file.length - 4);
+  }
+
+  String _getAssetPathFromFile(String file) {
+    return 'assets/$file';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Dalok"),
+        title: const Text('Songs'),
       ),
       body: FutureBuilder(
         future: loadSongs(),
         builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data!);
+          if (snapshot.connectionState == ConnectionState.done) {
+            return ListView.builder(
+              itemCount: _songs.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_songs[index].name),
+                );
+              },
+            );
           } else {
             return const Center(
               child: CircularProgressIndicator(),
