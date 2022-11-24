1f89985f533ca79e7e23d26e5c99d26bed81847d
Szabo Tamas <tamas.szabo0102@gmail.com>
2022-11-24 16:09:57 +0100

    Remove unused import

--- lib/viewmodels/authentication_viewmodel.dart
+++ lib/viewmodels/authentication_viewmodel.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:tiszapp_flutter/models/admin_api_data.dart';
 import 'package:tiszapp_flutter/models/user_data.dart';
 import 'package:tiszapp_flutter/services/api_service.dart';
