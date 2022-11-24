d2a40ce1c54e9a1cf576dbbd01353963d0945c1b
Szabo Tamas <tamas.szabo0102@gmail.com>
2022-11-24 21:13:44 +0100

    Upload scores vm refact

--- lib/viewmodels/scores_viewmodel.dart
+++ lib/viewmodels/scores_viewmodel.dart
@@ -1,11 +1,17 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:tiszapp_flutter/models/score_data.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class ScoresViewModel with ChangeNotifier {
   List<Score> scores = [];
 
   ScoresViewModel() {
+    initializeDateFormatting();
     _getScores();
   }
 
@@ -15,5 +21,31 @@ class ScoresViewModel with ChangeNotifier {
       scores.insert(0, Score.fromSnapshot(event.snapshot));
       notifyListeners();
     });
+  }
+
+  void uploadScore(
+      String name, String score1, String score2, String score3, String score4) {
+    var score = Score(
+      author: FirebaseAuth.instance.currentUser!.uid,
+      name: name,
+      score1: _scoreTextToInt(score1),
+      score2: _scoreTextToInt(score2),
+      score3: _scoreTextToInt(score3),
+      score4: _scoreTextToInt(score4),
+    );
+
+    var ref = FirebaseDatabase.instance.ref().child("debug/scores");
+    var now = DateTime.now();
+    var formatter = DateFormat('yyyyMMddHHmmssSSS');
+    var key = formatter.format(now);
+    ref.child(key).set(score.toJson());
+  }
+
+  int _scoreTextToInt(String text) {
+    if (text == "") {
+      return 0;
+    } else {
+      return int.tryParse(text) ?? 0;
+    }
   }
 }
--- lib/views/upload_score_screen.dart
+++ lib/views/upload_score_screen.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:tiszapp_flutter/colors.dart';
-import 'package:tiszapp_flutter/models/score_data.dart';
 import 'package:tiszapp_flutter/widgets/3d_button.dart';
 import 'package:tiszapp_flutter/widgets/input_field.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
+
+import '../viewmodels/scores_viewmodel.dart';
 
 class UploadScoreScreen extends StatelessWidget {
-  UploadScoreScreen({super.key}) {
-    initializeDateFormatting();
-  }
+  UploadScoreScreen({super.key});
 
   final _nameController = TextEditingController();
   final _score1Controller = TextEditingController();
   final _score2Controller = TextEditingController();
   final _score3Controller = TextEditingController();
   final _score4Controller = TextEditingController();
+  final _viewModel = ScoresViewModel();
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +50,6 @@ class UploadScoreScreen extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               Row(
-                //mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   SizedBox(
                     width: (MediaQuery.of(context).size.width - 70) / 4,
@@ -103,7 +98,13 @@ class UploadScoreScreen extends StatelessWidget {
                 children: [
                   Button3D(
                     onPressed: () {
-                      _uploadScore();
+                      _viewModel.uploadScore(
+                          _nameController.text,
+                          _score1Controller.text,
+                          _score2Controller.text,
+                          _score3Controller.text,
+                          _score4Controller.text);
+                      _clearFields();
                       _showDialog(context);
                     },
                     child: Text(
@@ -121,39 +122,6 @@ class UploadScoreScreen extends StatelessWidget {
             ],
           ),
         ));
-  }
-
-  void _uploadScore() {
-    var name = _nameController.text;
-    var score1 = _score1Controller.text;
-    var score2 = _score2Controller.text;
-    var score3 = _score3Controller.text;
-    var score4 = _score4Controller.text;
-
-    var score = Score(
-      author: FirebaseAuth.instance.currentUser!.uid,
-      name: name,
-      score1: _scoreTextToInt(score1),
-      score2: _scoreTextToInt(score2),
-      score3: _scoreTextToInt(score3),
-      score4: _scoreTextToInt(score4),
-    );
-
-    var ref = FirebaseDatabase.instance.ref().child("debug/scores");
-    var now = DateTime.now();
-    var formatter = DateFormat('yyyyMMddHHmmssSSS');
-    var key = formatter.format(now);
-    ref.child(key).set(score.toJson());
-
-    _clearFields();
-  }
-
-  int _scoreTextToInt(String text) {
-    if (text == "") {
-      return 0;
-    } else {
-      return int.tryParse(text) ?? 0;
-    }
   }
 
   void _clearFields() {
