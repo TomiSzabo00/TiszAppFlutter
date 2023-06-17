7659b2285d32ecabd59157e08130efeff80722b8
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-06-17 10:54:31 +0200

    Add database service

---
+++ lib/services/database_service.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_database/firebase_database.dart' as database;
+
+class DatabaseService {
+  static database.DatabaseReference ref =
+      database.FirebaseDatabase.instance.ref();
+
+  static Future<int> getNumberOfTeams() async {
+    final snapshot = await ref.child('number_of_teams').get();
+    if (snapshot.exists) {
+      return snapshot.value as int;
+    }
+    return 4;
+  }
+}
