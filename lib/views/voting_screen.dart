9686b98e87d6eaac8cad8f795556491f837f653f
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-06-17 10:25:38 +0200

    Add voting screen and viewModel

---
+++ lib/views/voting_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:tiszapp_flutter/viewmodels/voting_viewmodel.dart';
+import 'package:provider/provider.dart';
+
+class VotingScreen extends StatefulWidget {
+  const VotingScreen({Key? key}) : super(key: key);
+
+  @override
+  _VotingScreenState createState() => _VotingScreenState();
+}
+
+class _VotingScreenState extends State<VotingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final isDarkTheme = Theme.of(context).brightness == Brightness.dark;
+    final viewModel = context.watch<VotingViewmodel>();
+    return Container();
+  }
+}
