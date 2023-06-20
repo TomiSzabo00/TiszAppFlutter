33d06049f5f8b3cb52bfccee112fc8c3b28567f6
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-06-21 01:07:51 +0200

    Replace constant

--- lib/viewmodels/quiz_viewmodel.dart
+++ lib/viewmodels/quiz_viewmodel.dart
@@ -149,7 +149,7 @@ class QuizViewModel extends ChangeNotifier {
       final senderUid = event.snapshot.value as String;
       DatabaseService.getUserData(senderUid).then((senderData) {
         // check if this team already sent a signal
-        if (signals.contains(senderData.teamNum) || signals.length >= 6) {
+        if (signals.contains(senderData.teamNum) || signals.length >= numberOfTeams) {
           return;
         }
         signals.add(senderData.teamNum);
