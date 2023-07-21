42379c2163efd4420362dfd015dbd550c3de4bf2
Szabo Tamas <tamas.szabo0102@gmail.com>
2023-07-21 16:56:23 +0300

    Get maps api key from friebase

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,14 +4,29 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
 
-    GMSServices.provideAPIKey("AIzaSyAISIjQmAo9yNuykep3orB0k1gjIYTMetk")
+        readDataFromFirebase { apiKey in
+            GMSServices.provideAPIKey(apiKey)
+        }
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    func readDataFromFirebase(callback: @escaping (String) -> Void) -> Void {
+        let ref = Database.database().reference()  // Root reference to your Firebase Database
+
+        // Assuming your API key is stored under "api_key" node
+        ref.child("api_key").observeSingleEvent(of: .value) { snapshot in
+            if let apiKey = snapshot.value as? String {
+                callback(apiKey)
+            } else {
+                callback("")
+            }
+        }
+    }
 }
