54583e5fa02ea8a51309ec58a8f106d51836f6c8
JuhCsabi <c.juhasz@student.utwente.nl>
2023-07-21 15:41:37 +0200

    New Maps API key

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
 
-    GMSServices.provideAPIKey("AIzaSyAISIjQmAo9yNuykep3orB0k1gjIYTMetk")
+    GMSServices.provideAPIKey("AIzaSyBHXc_JfbEYblfPcoIhjyFiAq0HdHzwWzw")
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
