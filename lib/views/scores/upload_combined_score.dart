b5544a1e201a786192b08d7518545b03638d092a
Szabo Tamas <tamas.szabo0102@gmail.com>
2024-07-16 00:36:49 +0200

    Update style

--- lib/views/scores/upload_combined_score.dart
+++ lib/views/scores/upload_combined_score.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiszapp_flutter/colors.dart';
 import 'package:tiszapp_flutter/views/scores/upload_ocsi_score_screen.dart';
 import 'package:tiszapp_flutter/views/scores/upload_score_screen.dart';
 import 'package:tiszapp_flutter/views/scores/upload_style_point_screen.dart';
@@ -13,17 +14,21 @@ class UploadCombinedScoreScreen extends StatefulWidget {
 class UploadCombinedScoreScreenState extends State<UploadCombinedScoreScreen> {
   @override
   Widget build(BuildContext context) {
+    bool isDarkTheme = MediaQuery.of(context).platformBrightness == Brightness.dark;
     return DefaultTabController(
       length: 3,
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Pontozás"),
-          bottom: const TabBar(
-            tabs: [
+          bottom: TabBar(
+            tabs: const [
               Tab(text: "Sima"),
               Tab(text: "Öcsi"),
               Tab(text: "Stíluspont"),
             ],
+            indicatorColor: isDarkTheme ? CustomColor.btnTextNight : CustomColor.btnTextDay,
+            labelColor: isDarkTheme ? CustomColor.btnTextNight : CustomColor.btnTextDay,
+            unselectedLabelColor: isDarkTheme ? CustomColor.btnTextNight : CustomColor.btnTextDay,
           ),
         ),
         body: const TabBarView(
