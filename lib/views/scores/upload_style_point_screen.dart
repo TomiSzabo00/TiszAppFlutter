49cf59de134dac4be66afd6bbf2a08764dbc82dc
Szabo Tamas <tamas.szabo0102@gmail.com>
2024-07-15 22:34:42 +0200

    Add ui

--- lib/views/scores/upload_style_point_screen.dart
+++ lib/views/scores/upload_style_point_screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:tiszapp_flutter/viewmodels/style_points_viewmodel.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:tiszapp_flutter/colors.dart';
