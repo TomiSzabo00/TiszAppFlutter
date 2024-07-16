9e75dc1e180fa65bd270050c98e23a1738b4decd
mozsarmate <mozsarmatee@gmail.com>
2024-07-16 12:57:29 +0200

    old buttons removed icons updated

--- lib/viewmodels/main_menu_viewmodel.dart
+++ lib/viewmodels/main_menu_viewmodel.dart
@@ -22,12 +22,15 @@ import 'package:tiszapp_flutter/views/quiz/quiz_screen.dart';
 import 'package:tiszapp_flutter/views/quiz/slow_quiz_screen.dart';
 import 'package:tiszapp_flutter/views/song_request_screen.dart';
 import 'package:tiszapp_flutter/views/schedule_screen.dart';
+import 'package:tiszapp_flutter/views/scores/upload_combined_score.dart';
 import 'package:tiszapp_flutter/views/scores_screen.dart';
 import 'package:tiszapp_flutter/views/songs_screen.dart';
 import 'package:tiszapp_flutter/views/sports_result_view_screen.dart';
 import 'package:tiszapp_flutter/views/sports_screen.dart';
 import 'package:tiszapp_flutter/views/texts_screen.dart';
 import 'package:tiszapp_flutter/views/scores/upload_combined_score.dart';
+import 'package:tiszapp_flutter/views/scores/upload_ocsi_score_screen.dart';
+import 'package:tiszapp_flutter/views/scores/upload_score_screen.dart';
 import 'package:tiszapp_flutter/views/upload_texts_screen.dart';
 import 'package:tiszapp_flutter/views/voting_screen.dart';
 import 'package:tiszapp_flutter/views/wordle_screen.dart';
@@ -172,8 +175,6 @@ class MainMenuViewModel extends ChangeNotifier {
       MainMenuButton(type: MainMenuButtonType.quizQuick),
       MainMenuButton(type: MainMenuButtonType.slowQuiz),
       MainMenuButton(type: MainMenuButtonType.combinedScoreUpload),
-      MainMenuButton(type: MainMenuButtonType.scoreUpload),
-      MainMenuButton(type: MainMenuButtonType.ocsiScoreUpload),
       MainMenuButton(type: MainMenuButtonType.voting),
       MainMenuButton(type: MainMenuButtonType.ejjeliportya),
       MainMenuButton(type: MainMenuButtonType.notifications),
@@ -206,8 +207,6 @@ class MainMenuViewModel extends ChangeNotifier {
       return MainMenuButtonType.quizQuick;
     } else if (key == MainMenuButtonType.schedule.rawValue) {
       return MainMenuButtonType.schedule;
-    } else if (key == MainMenuButtonType.scoreUpload.rawValue) {
-      return MainMenuButtonType.scoreUpload;
     } else if (key == MainMenuButtonType.combinedScoreUpload.rawValue) {
       return MainMenuButtonType.combinedScoreUpload;
     } else if (key == MainMenuButtonType.scores.rawValue) {
@@ -337,12 +336,6 @@ class MainMenuViewModel extends ChangeNotifier {
         return () => Navigator.of(context).push(
               MaterialPageRoute(
                 builder: (context) => const ScheduleScreen(),
-              ),
-            );
-      case MainMenuButtonType.scoreUpload:
-        return () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => const UploadScoreScreen(),
               ),
             );
       case MainMenuButtonType.scores:
