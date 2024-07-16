abe598684106e8be4255c564f029655844e738e7
Szabo Tamas <tamas.szabo0102@gmail.com>
2024-07-16 21:56:36 +0200

    WIP

--- lib/viewmodels/audience_voting_viewmodel.dart
+++ lib/viewmodels/audience_voting_viewmodel.dart
@@ -1,3 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
+import 'package:tiszapp_flutter/helpers/try_cast.dart';
+import 'package:tiszapp_flutter/services/database_service.dart';
 
-class AudienceVotingViewModel extends ChangeNotifier {}
+class AudienceVotingViewModel extends ChangeNotifier {
+  final DatabaseReference database = DatabaseService.database;
+  final databaseNode = 'audience_votes';
+  String selectedOption = '';
+
+  Stream<bool> isVotingOpen() async* {
+    yield* database.child('$databaseNode/voting_state').onValue.map((event) {
+      return tryCast<bool>(event.snapshot.value) ?? false;
+    });
+  }
+
+  Stream<bool> didUserVote() async* {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid == null) {
+      yield true;
+    }
+
+    yield* database.child('$databaseNode/votes/$uid').onValue.map((event) {
+      return event.snapshot.exists;
+    });
+  }
+
+  Stream<bool> isResultVisible() async* {
+    yield* database.child('$databaseNode/result_showing').onValue.map((event) {
+      return tryCast<bool>(event.snapshot.value) ?? false;
+    });
+  }
+
+  Stream<List<String>> getVotingOptions() async* {
+    yield* database.child('$databaseNode/voting_options').onValue.map((event) {
+      final rawOptions = event.snapshot.value;
+      if (rawOptions != null && rawOptions is Map && rawOptions.isNotEmpty) {
+        return rawOptions.values.cast<String>().toList();
+      }
+      return [];
+    });
+  }
+
+  void selectOption(String option) {
+    selectedOption = option;
+    notifyListeners();
+  }
+
+  bool vote() {
+    final uid = FirebaseAuth.instance.currentUser?.uid;
+    if (uid == null || selectedOption.isEmpty) {
+      return false;
+    }
+
+    database.child('$databaseNode/votes/$uid').set(selectedOption);
+    return true;
+  }
+}
--- lib/views/audience_voting_screen.dart
+++ lib/views/audience_voting_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tiszapp_flutter/helpers/try_cast.dart';
+import 'package:tiszapp_flutter/viewmodels/audience_voting_viewmodel.dart';
 
 class AudienceVotingScreen extends StatefulWidget {
   const AudienceVotingScreen({
@@ -15,11 +18,117 @@ class AudienceVotingScreen extends StatefulWidget {
 class AudienceVotingScreenState extends State<AudienceVotingScreen> {
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<AudienceVotingViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Közönségszavazás'),
       ),
-      body: Container(),
+      body: widget.isAdmin ? _userScreen(viewModel) : _userScreen(viewModel),
+    );
+  }
+
+  Widget _userScreen(AudienceVotingViewModel viewModel) {
+    return StreamBuilder(
+      stream: viewModel.isVotingOpen(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final bool isOpen = tryCast<bool>(snapshot.data) ?? false;
+          if (isOpen) {
+            return StreamBuilder(
+              stream: viewModel.didUserVote(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final bool didVote = tryCast<bool>(snapshot.data) ?? true;
+                  if (didVote) {
+                    return StreamBuilder(
+                      stream: viewModel.isResultVisible(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          final bool isResultVisible = tryCast<bool>(snapshot.data) ?? false;
+                          if (isResultVisible) {
+                            return _resultsVisibleScreen();
+                          }
+                          return _resultsHiddenScreen();
+                        }
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    );
+                  }
+                  return _votingScreen(viewModel);
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            );
+          }
+          return _votingClosedScreen();
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  Widget _adminScreen(AudienceVotingViewModel viewModel) {
+    return Container();
+  }
+
+  Widget _votingClosedScreen() {
+    return const Center(child: Text('A szavazás jelenleg nincs nyitva.'));
+  }
+
+  Widget _resultsHiddenScreen() {
+    return const Center(child: Text('A szavazatodat sikeresen rögzítettük!'));
+  }
+
+  Widget _resultsVisibleScreen() {
+    // TODO
+    return const Center(child: Text('A szavazás eredményeit megtekintheted!'));
+  }
+
+  Widget _votingScreen(AudienceVotingViewModel viewModel) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('Válaszd ki azt az EGY párost, akik szerinted a legjobbak voltak:'),
+          const SizedBox(height: 10),
+          StreamBuilder(
+            stream: viewModel.getVotingOptions(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final List<String> options = snapshot.data ?? [];
+                if (options.isEmpty) {
+                  return const Center(child: Text('Nincsenek lehetőségek. :('));
+                }
+                return SingleChildScrollView(
+                  child: Column(
+                    children: options.map((option) {
+                      return RadioListTile(
+                        title: Text(option),
+                        value: option,
+                        groupValue: viewModel.selectedOption,
+                        onChanged: (_) {
+                          viewModel.selectOption(option);
+                        },
+                        selected: viewModel.selectedOption == option,
+                      );
+                    }).toList(),
+                  ),
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
